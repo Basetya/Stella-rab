8d86b8af828bea20c9af54bc358dda52469002b2
Basetya <bambang.setyanto@gmail.com>
2026-05-26 00:59:47 +0700

    Fix case-sensitivity mismatch for index template

--- src/Code.js
+++ src/Code.js
@@ -1,5 +1,5 @@
 function doGet() {
-  return HtmlService.createTemplateFromFile('Index').evaluate().setTitle("Stella's RAB Portal").addMetaTag('viewport', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no').setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return HtmlService.createTemplateFromFile('index').evaluate().setTitle("Stella's RAB Portal").addMetaTag('viewport', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no').setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 function include(filename) { return HtmlService.createHtmlOutputFromFile(filename).getContent(); }
 
